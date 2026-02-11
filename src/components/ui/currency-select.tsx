7657afaf9662417e26e0772bc04a7e166080981b
iwanhkim <ikhwanulhkim@gmai.com>
2026-02-11 12:50:02 +0800

    feat(ui): add shadcn/ui base components

---
+++ src/components/ui/currency-select.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { CURRENCIES, type CurrencyCode } from "@/lib/currencies";
+
+interface CurrencySelectProps {
+  value: CurrencyCode;
+  onValueChange: (value: CurrencyCode) => void;
+  disabled?: boolean;
+}
+
+export function CurrencySelect({ value, onValueChange, disabled }: CurrencySelectProps) {
+  return (
+    <Select value={value} onValueChange={(v) => onValueChange(v as CurrencyCode)} disabled={disabled}>
+      <SelectTrigger>
+        <SelectValue placeholder="Pilih mata uang" />
+      </SelectTrigger>
+      <SelectContent>
+        {CURRENCIES.map((c) => (
+          <SelectItem key={c.code} value={c.code}>
+            {c.flag} {c.code} - {c.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
